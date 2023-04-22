db83396f1be91e1c9f4f2790c10cae31aac390a9
Chris Allen <gh.84kjuqg365@gmail.com>
2023-04-22 13:37:55 -0700

    Create basic web service

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/BruteForceHashcode")]
+fn brute_force_hashcode() -> &'static str {
+    "got it!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes!(brute_force_hashcode))
 }
